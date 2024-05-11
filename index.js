1109e322cf16c8e0cc29abee2798e0b3e2d47986
Muin-Uddin-Ahmad <muinuddin.sm12@gamil.com>
2024-05-11 08:53:11 +0600

    assignments post and get method emplemented

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 7000
 const app = express()
@@ -14,6 +15,55 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ff1pkvw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const assignmentsCollection = client.db('assignmentsDB').collection('assignments')
+
+    // save a assignment in db 
+    app.post('/assignments', async(req, res) => {
+        const assignmentData = req.body
+        const result = await assignmentsCollection.insertOne(assignmentData)
+        res.send(result)
+    })
+    app.get('/assignments', async(req, res) => {
+        const result = await assignmentsCollection.find().toArray()
+        res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello from EduCircle Server....')
